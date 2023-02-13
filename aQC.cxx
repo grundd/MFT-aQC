5939d0a45fe3671c8aeea1beebf9457130c7a48a
David <david.grund@cern.ch>
2023-02-13 12:26:16 +0100

    first commit

---
+++ aQC.cxx
@@ -0,0 +1,178 @@
+// aQC.cxx
+// David Grund, Jan 18, 2023
+// MFT asynchronous Quality Control
+
+// cpp
+#include <vector>
+#include <fstream>
+// root
+#include "TSystem.h"
+#include "TFile.h"
+#include "TH1.h"
+#include "TH2.h"
+
+bool rewriteAll = false;
+bool oldPath = false;
+
+vector<int>* ReadInput(string sIn, string &period, string &pass1, string &pass2, string &pass3)
+{
+    std::vector<int>* runList = new std::vector<int>;
+    ifstream ifs;
+    ifs.open(sIn);
+    int nRuns;
+    ifs >> period
+        >> pass1
+        >> pass2
+        >> pass3
+        >> nRuns;
+    for(int i = 0; i < nRuns; i++) {
+        int run;
+        ifs >> run;
+        runList->push_back(run);
+    }
+    cout << "Input:\n"
+         << "period: " << period << "\n"
+         << "pass1:  " << pass1 << "\n"
+         << "pass2:  " << pass2 << "\n"
+         << "pass3:  " << pass3 << "\n"
+         << "run list (" << nRuns << " runs):\n";
+    for(int i = 0; i < nRuns; i++) cout << Form("%03i -> ",i+1) << runList->at(i) << "\n";
+    return runList;
+}
+
+template <typename TH>
+TH* LoadHisto(string path, string histName, int runNo, string pass)
+{
+    o2::ccdb::CcdbApi api;
+    if(pass == "passMC") api.init("ccdb-test.cern.ch:8080");
+    else                 api.init("ali-qcdb-gpn.cern.ch:8083");
+    map<string, string> metadata;
+    metadata["RunNumber"] = std::to_string(runNo);
+    if(pass != "online") metadata["PassName"] = pass;
+    TH* h = api.retrieveFromTFileAny<TH>(path+histName,metadata,-1); // timestamp: -1
+    if(h) return h;
+    else  return NULL;
+}
+
+string RenameHisto(string oldName)
+{
+    string newName = oldName;
+    char slash = '/';
+    size_t index = newName.find_last_of(slash);
+    if (index < string::npos) {
+        // erase everything up to 'index'
+        newName.erase(0,index+1);
+    }
+    return newName;
+}
+
+void SaveHistos(string period, int runNo, string pass)
+{
+    gSystem->Exec(Form("mkdir -p Results/%s/runsRootFiles/",period.data()));
+    // online QC?
+    bool online = false;
+    if(pass == "online") online = true;
+    // MC?
+    bool isMC = false;
+    if(pass == "passMC") isMC = true;
+    // check if the file already exists
+    string sFile = Form("Results/%s/runsRootFiles/%i_%s.root",period.data(),runNo,pass.data());
+    bool fileExists = !gSystem->AccessPathName(sFile.data());
+    if(fileExists && !rewriteAll) {
+        cout << sFile << " already exists and won't be recreated. Skipping...\n";
+    } else {
+        cout << sFile << " doesn't exist or will be recreated. Histograms will be loaded now:\n";
+        TFile* f = new TFile(sFile.data(),"recreate");
+        string sPath = "";
+        if(isMC) {
+            if(!online) sPath += "qc_mc/";
+            else { 
+                cout << "Online QC not available for MC. Terminating...\n";
+                return;
+            }
+        } else {
+            if(!online) sPath += "qc_async/";
+            else        sPath += "qc/";
+        }    
+        sPath += "MFT/MO/";
+        string subfldr = "Tracks/";
+        if(oldPath) subfldr = "QcMFTAsync/";
+        if(online) subfldr = "MFTAsyncTask/";
+        std::vector<string> sNamesTH2 = {
+            // track position
+            subfldr+"tracks/mMFTTrackEtaPhi_5_MinClusters",
+            subfldr+"tracks/mMFTTrackXY_5_MinClusters",
+            subfldr+"tracks/mMFTTrackEtaPhi_7_MinClusters",
+            subfldr+"tracks/mMFTTrackXY_7_MinClusters",
+            subfldr+"tracks/mMFTTrackEtaPhi_8_MinClusters",
+            subfldr+"tracks/mMFTTrackXY_8_MinClusters"
+        };
+        if(isMC) {
+            sNamesTH2.push_back("Digits/mDigitOccupancySummary");
+            sNamesTH2.push_back("Clusters/mClusterOccupancySummary");
+        }
+        std::vector<string> sNamesTH1 = {
+            // tracks
+            subfldr+"tracks/mMFTTrackEta",
+            subfldr+"tracks/mMFTTrackNumberOfClusters",
+            subfldr+"tracks/mMFTTrackPhi",
+            subfldr+"tracks/mMFTTrackTanl",
+            subfldr+"tracks/mNOfTracksTime",
+            subfldr+"tracks/mMFTTrackInvQPt",
+            subfldr+"tracks/CA/mMFTCATrackPt",
+            subfldr+"tracks/LTF/mMFTLTFTrackPt",
+            subfldr+"tracks/CA/mMFTCATrackEta",
+            subfldr+"tracks/LTF/mMFTLTFTrackEta",
+            subfldr+"tracks/mMFTTracksBC",
+            // clusters
+            subfldr+"clusters/mMFTClusterPatternIndex",
+            subfldr+"clusters/mMFTClusterSensorIndex",
+            subfldr+"clusters/mMFTClustersROFSize",
+            subfldr+"clusters/mNOfClustersTime"
+        };
+        if(isMC) {
+            sNamesTH1.push_back("Digits/mDigitChipOccupancy");
+            sNamesTH1.push_back("Digits/mDigitsBC");
+            sNamesTH1.push_back("Clusters/mClusterOccupancy");
+            sNamesTH1.push_back("Clusters/mClusterSizeSummary");
+            sNamesTH1.push_back("Clusters/mClusterZ");
+            sNamesTH1.push_back("Clusters/mGroupedClusterSizeSummary");
+        }
+        for(int i = 0; i < sNamesTH2.size(); i++) {
+            TH2F* h = LoadHisto<TH2F>(sPath,sNamesTH2[i],runNo,pass);
+            if(h) {
+                cout << "run " << runNo << ", " << pass << ": " << h->GetName() << " loaded\n";
+                f->cd();
+                h->Write(RenameHisto(h->GetName()).data());
+            }
+        }
+        for(int i = 0; i < sNamesTH1.size(); i++) {
+            TH1F* h = LoadHisto<TH1F>(sPath,sNamesTH1[i],runNo,pass);
+            if(h) {
+                cout << "run " << runNo << ", " << pass << ": " << h->GetName() << " loaded\n";
+                f->cd();
+                h->Write(RenameHisto(h->GetName()).data());
+            }
+        }
+        f->Write("",TObject::kWriteDelete);
+        f->Close();
+    }
+    return;
+}
+
+void aQC(string sIn)
+{
+    string period;
+    string pass1;
+    string pass2;
+    string pass3;
+    std::vector<int>* runList = ReadInput(sIn,period,pass1,pass2,pass3);
+    int nRuns = runList->size();
+    // QC of the first pass
+    for(int i = 0; i < nRuns; i++) SaveHistos(period.data(),runList->at(i),pass1);
+    // QC of the second pass (if any)
+    if(pass2 != "none") for(int i = 0; i < nRuns; i++) SaveHistos(period.data(),runList->at(i),pass2);
+    // if not MC -> online QC
+    if(pass3 != "none") for(int i = 0; i < nRuns; i++) SaveHistos(period.data(),runList->at(i),pass3);
+    return;
+}
---
+++ aQC_runAll.sh
@@ -0,0 +1,109 @@
+# shell script must be first allowed: chmod +x aQC_run.sh
+#!/bin/bash
+# to run it do (inside ali shell):
+# ./aQC_run.sh
+
+inputFile='singleRun.txt'
+## aQC meeting 2023-01-24:
+#inputFile='LHC22i1_passMC_O2-3486.txt'
+#inputFile='LHC22o_apass2_O2-3496_group1.txt'
+#inputFile='LHC22o_apass2_O2-3496_group2.txt'
+## aQC meeting 2023-01-31:
+#inputFile='LHC22o_apass2_O2-3496_group3.txt'
+#inputFile='LHC22r_apass2_O2-3526_group1.txt'
+## aQC meeting 2023-02-07:
+#inputFile='LHC22p_apass2_O2-3529_group1.txt'
+#inputFile='LHC22r_apass2_O2-3526_group2.txt'
+#inputFile='LHC22t_apass2_O2-3530_group1.txt'
+## aQC meeting 2023-02-14:
+#inputFile='LHC22o_apass2_O2-3496_group4a.txt'
+#inputFile='LHC22o_apass2_O2-3496_group4b.txt'
+#inputFile='LHC22o_apass2_O2-3496_group4c.txt'
+#inputFile='LHC22o_apass2_test_O2-3496.txt'
+#inputFile='LHC22o_HIR_apass2_O2-3496.txt'
+#inputFile='LHC22q_apass2_O2-3548_group1.txt'
+#inputFile='LHC22r_apass2_O2-3526_group3.txt'
+#inputFile='LHC22t_apass2_O2-3530_group2.txt'
+## aQC meeting 2023-02-21:
+
+root -q 'aQC.cxx("'$inputFile'")'
+
+n=1 
+period=""
+pass1=""
+pass2=""
+pass3=""
+nRuns=0
+run=500000
+currentPass=""
+
+printNotebook_singlePass () {
+    file=MFTaQC_${run}_${currentPass}
+    papermill -p _period $period -p _pass $currentPass -p _run $run notebook_aQC_singlePass.ipynb ${file}.ipynb
+    if [ $currentPass = "passMC" ] ; then
+        jupyter nbconvert --to html ${file}.ipynb --no-input 
+    else
+        jupyter nbconvert --to html ${file}.ipynb --no-input --TagRemovePreprocessor.remove_cell_tags onlyMC
+    fi
+    rm -r ${file}.ipynb
+    mv ${file}.html Results/${period}/notebook_singlePass/${run}_${currentPass}.html
+}
+
+while read line; do
+    if (( $n == 1 )) ; then
+        period=$line
+        echo "period:  $period"
+    elif (( $n == 2 )) ; then
+        pass1=$line
+        echo "pass1:   $pass1"
+    elif (( $n == 3 )) ; then
+        pass2=$line
+        echo "pass2:   $pass2"
+    elif (( $n == 4 )) ; then
+        pass3=$line
+        echo "pass3:   $pass3"
+    elif (( $n == 5 )) ; then
+        nRuns=$line
+        echo "# runs:  $nRuns"
+    else
+        run=$line
+        echo "run: $run"
+        # QC plots for individual passes
+        mkdir -p Results/${period}/notebook_singlePass/
+        # pass one
+        if [ $pass1 != "none" ] ; then
+            currentPass=$pass1
+            printNotebook_singlePass
+        fi
+        # pass two
+        if [ $pass2 != "none" ] ; then
+            currentPass=$pass2
+            printNotebook_singlePass
+        fi
+        # pass three
+        if [ $pass3 != "none" ] ; then
+            currentPass=$pass3
+            printNotebook_singlePass
+        fi
+        # if other passes available, do the comparison
+        if [ $pass2 = "none" ] ; then
+            echo "no other passes"
+        else
+            # comparison of the two passes
+            mkdir -p Results/${period}/notebook_compPasses/
+            file=MFTaQC_${run}_${pass1}_vs_${pass2}
+            papermill -p _period $period -p _pass1 $pass1 -p _pass2 $pass2 -p _pass3 $pass3 -p _run $run notebook_aQC_compPasses.ipynb ${file}.ipynb
+            jupyter nbconvert --to html ${file}.ipynb --no-input
+            rm -r ${file}.ipynb
+            mv ${file}.html Results/${period}/notebook_compPasses/${run}_${pass1}_vs_${pass2}.html
+        fi
+    fi
+    n=$((n+1))
+done < $inputFile
+
+# https://stackoverflow.com/questions/36901154/how-export-a-jupyter-notebook-to-html-from-the-command-line
+# https://stackoverflow.com/questions/49907455/hide-code-when-exporting-jupyter-notebook-to-html
+# https://www.wrighters.io/parameters-jupyter-notebooks-with-papermill/
+# https://pypi.org/project/jupyter-runner/ 
+# https://stackoverflow.com/questions/31517194/how-to-hide-one-specific-cell-input-or-output-in-ipython-notebook
+
